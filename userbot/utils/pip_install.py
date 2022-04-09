7df194628d7ad74e6adba41e70bf216dc01b4f1c
Arazz <91001726+Arazzq@users.noreply.github.com>
2022-04-09 20:03:40 +0400

    Add files via upload

---
+++ userbot/utils/pip_install.py
@@ -0,0 +1,11 @@
+# Mia UserBot - Ч ⁪⁬⁮⁮
+
+from subprocess import PIPE, Popen
+
+
+def install_pip(pipfile):
+    print(f"installing {pipfile}")
+    pip_cmd = ["pip", "install", f"{pipfile}"]
+    process = Popen(pip_cmd, stdout=PIPE, stderr=PIPE)
+    stdout, stderr = process.communicate()
+    return stdout
