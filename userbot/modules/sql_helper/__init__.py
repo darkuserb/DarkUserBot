d8678283d56ca8851a145f73a8312c0c670ac7fa
bossuserb <103330740+bossuserb@users.noreply.github.com>
2022-04-11 00:21:24 +0400

    Add files via upload

---
+++ userbot/modules/sql_helper/__init__.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
+#from sqlalchemy.dialects import *
+
+from userbot import DB_URI
+
+BASE = declarative_base()
+
+
+DB_URI = DB_URI.replace('postgres://','postgresql://',1)
+
+def start() -> scoped_session:
+    engine = create_engine(DB_URI)
+    BASE.metadata.bind = engine
+    BASE.metadata.create_all(engine)
+    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+
+
+SESSION = start()
