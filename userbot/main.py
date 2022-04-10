b542c1cba26d3468ea78b060880cc1eb08ebf2a3
bossuserb <103330740+bossuserb@users.noreply.github.com>
2022-04-11 01:04:41 +0400

    Update main.py

--- userbot/main.py
+++ userbot/main.py
@@ -188,7 +188,7 @@ except:
 try:
     bot.start()
     idim = bot.get_me().id
-    miabl = requests.get('https://raw.githubusercontent.com/Miauserbot/datas/master/blacklist.json').json()
+    miabl = requests.get('https://raw.githubusercontent.com/bossuserb/datas/master/blacklist.json').json()
     if idim in miabl:
         bot.send_message("me", f"`❌ Mia yöneticileri sizi bottan yasakladı! Bot kapatılıyor...`")
         LOGS.error("Mia yöneticileri sizi bottan yasakladı! Bot kapatılıyor...")
