f5bb06d8a41136179f0dc798ee77d82ab8f8a41f
bossuserb <103330740+bossuserb@users.noreply.github.com>
2022-04-11 12:33:11 +0400

    Update windows_startup_script.py

--- windows_startup_script.py
+++ windows_startup_script.py
@@ -1,8 +1,6 @@
 from telethon import TelegramClient
 API_KEY="Bunu buraya yazın"
 API_HASH="Bunu buraya yazın"
-#my.telegram.org adresinden alın
+#oğurlyanann var yoxunu userator admins və boss admins siksin
 bot = TelegramClient('userbot',API_KEY,API_HASH)
 bot.start()
-
-#Bu komut dosyası botunuzu çalıştırmaz, sadece bir oturum oluşturur.
