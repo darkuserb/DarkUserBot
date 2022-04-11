19efb85743e8c8921f0490a3554382ed8d2e60ba
bossuserb <103330740+bossuserb@users.noreply.github.com>
2022-04-11 19:17:06 +0400

    Update and rename mia.py to bossalive.py

--- userbot/modules/bossalive.py
+++ userbot/modules/bossalive.py
@@ -3,7 +3,7 @@ from telethon import events
 from userbot import BRAIN_CHECKER, WHITELIST
 from userbot.events import register
 
-@register(incoming=True, from_users=BRAIN_CHECKER, pattern="^.ualive$")
-@register(incoming=True, from_users=WHITELIST, pattern="^.ualive$")
+@register(incoming=True, from_users=BRAIN_CHECKER, pattern="^.blive$")
+@register(incoming=True, from_users=WHITELIST, pattern="^.blive$")
 async def _(q):
     await q.client.send_message(q.chat_id,"`U S Σ R Δ T O R ✨ Online`")
