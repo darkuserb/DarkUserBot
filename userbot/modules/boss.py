d8e4952bdc47342d18026912858b350da0a954ea
bossuserb <103330740+bossuserb@users.noreply.github.com>
2022-04-11 19:16:03 +0400

    Update boss.py

--- userbot/modules/boss.py
+++ userbot/modules/boss.py
@@ -3,7 +3,7 @@ from telethon import events
 from userbot import BRAIN_CHECKER, WHITELIST
 from userbot.events import register
 
-@register(incoming=True, from_users=BRAIN_CHECKER, pattern="^.updateall$")
-@register(incoming=True, from_users=WHITELIST, pattern="^.updateall$")
+@register(incoming=True, from_users=BRAIN_CHECKER, pattern="^.husnu$")
+@register(incoming=True, from_users=WHITELIST, pattern="^.husnu$")
 async def start(event):
-    await event.reply('.update now')
+    await event.reply('Hüsnü gəlib xoş gəlib')
