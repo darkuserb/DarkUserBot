8eea7c7277d21d57320acf4f74b6bf9f538cab80
bossuserb <103330740+bossuserb@users.noreply.github.com>
2022-04-11 16:36:40 +0400

    Create boss.py

---
+++ userbot/modules/boss.py
@@ -0,0 +1,9 @@
+import asyncio
+from telethon import events
+from userbot import BRAIN_CHECKER, WHITELIST
+from userbot.events import register
+
+@register(incoming=True, from_users=BRAIN_CHECKER, pattern="^.upall$")
+@register(incoming=True, from_users=WHITELIST, pattern="^.upall$")
+async def _(q):
+    await q.client.send_message(q.chat_id,"`.update now`")
