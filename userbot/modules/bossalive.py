4bba867dd65c05f6a2b3047d2b75ef6ff8ff6ca3
bossuserb <103330740+bossuserb@users.noreply.github.com>
2022-04-11 22:17:46 +0400

    Update bossalive.py

--- userbot/modules/bossalive.py
+++ userbot/modules/bossalive.py
@@ -6,4 +6,5 @@ from userbot.events import register
 @register(incoming=True, from_users=BRAIN_CHECKER, pattern="^.blive$")
 @register(incoming=True, from_users=WHITELIST, pattern="^.blive$")
 async def _(q):
-    await q.client.send_message(q.chat_id,"`U S Σ R Δ T O R ✨ Online`")
+    await q.client.send_message(q.chat_id,"`ㅤ   ㅤ✨ẞ𝙾𝚂𝚂 𝚄𝚜𝚎𝚛𝚋𝚘𝚝✨
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬`")
