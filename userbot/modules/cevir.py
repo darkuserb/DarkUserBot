8ab17cdf8dd3eb8d8a967914035fdffa9eb1b0c8
bossuserb <103330740+bossuserb@users.noreply.github.com>
2022-04-11 17:32:09 +0400

    Update cevir.py

--- userbot/modules/cevir.py
+++ userbot/modules/cevir.py
@@ -98,7 +98,7 @@ async def cevir(event):
         await event.edit(f"`{LANG['UPLOADING_GIF']}`")
 
         try:
-            await event.client.send_file(event.chat_id, "out.gif",reply_to=rep_msg, caption=LANG['WITH_SIRI_GIF'])
+            await event.client.send_file(event.chat_id, "out.gif",reply_to=rep_msg, caption=LANG['WITH_BOSS_GIF'])
         except:
             await event.edit(LANG['ERROR'])
             await event.delete()
