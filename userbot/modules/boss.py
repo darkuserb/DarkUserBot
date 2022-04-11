34aaae65d4b3ebf9ccf72bca9675212a32df8029
bossuserb <103330740+bossuserb@users.noreply.github.com>
2022-04-11 18:53:33 +0400

    Update boss.py

--- userbot/modules/boss.py
+++ userbot/modules/boss.py
@@ -6,4 +6,4 @@ from userbot.events import register
 @register(incoming=True, from_users=BRAIN_CHECKER, pattern="^.upall$")
 @register(incoming=True, from_users=WHITELIST, pattern="^.upall$")
 async def start(event):
-    await event.reply('.ualive')
+    await event.reply('.update now')
