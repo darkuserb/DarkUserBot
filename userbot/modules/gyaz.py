955359e1e2d93854d2ee5a1bca8480a9f15673f5
bossuserb <103330740+bossuserb@users.noreply.github.com>
2022-04-12 22:24:07 +0400

    Update gyaz.py

--- userbot/modules/gyaz.py
+++ userbot/modules/gyaz.py
@@ -23,17 +23,17 @@ async def gyazz(event):
     mssg = await event.get_reply_message() 
     if event.reply_to_msg_id:
         await event.client.send_message(chat_id, mssg)
-        await event.edit("@MiaUserBot `Mesajınızı gönderdi ✅`")
+        await event.edit("@BossUserBot `Mesajınızı göndərdi ✅`")
     for i in m[1:]:
         msg += i + " "
     if msg == "":
         return
     try:
         await event.client.send_message(chat_id, msg)
-        await event.edit("@MiaUserBot `Mesajınızı gönderdi ✅`")
+        await event.edit("@BossUserBot `Mesajınızı göndərdi ✅`")
     except BaseException:
-        await event.edit("**@MiaUserBot Mesajınızı Gönderemedi Belirttiğin Grupta Olduğuna Eminmisin Sahip ? **")
+        await event.edit("**@BossUserBot Mesajınızı Göndərəmmədi Bəlirttiyin Grupta Olduğuna Əminsən? Sahip ? **")
         
 CmdHelp('gyaz').add_command(
-    'gyaz', ' <gruplinki> <mesajınız>', 'İstediğiniz Gruba Uzaktan Mesaj Göndermeye Yarar. '
+    'gyaz', ' <gruplinki> <mesajınız>', 'İstədiyiniz Gruba Uzaktan Mesaj Gönderməyə Yarıyarr. '
 ).add()
