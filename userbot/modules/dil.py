18d76e7728e1a34bcd3d60a58edf57df5c94d8e8
bossuserb <103330740+bossuserb@users.noreply.github.com>
2022-04-12 20:35:55 +0400

    Update dil.py

--- userbot/modules/dil.py
+++ userbot/modules/dil.py
@@ -13,20 +13,20 @@ async def dil(event):
 
     komut = event.pattern_match.group(1)
     if search(r"y[uü]kle|install", komut):
-        await event.edit("`Dil dosyası yükleniyor...`")
+        await event.edit("`Dil faylı yüklənir...`")
         if event.is_reply:
             reply = await event.get_reply_message()
             dosya = await reply.download_media()
 
             if ((len(reply.file.name.split(".")) >= 2) and (not reply.file.name.split(".")[1] == "sirijson")):
-                return await event.edit("`Lütfen geçerli bir` **MiaJSON** `dosyası verin!`")
+                return await event.edit("`Xahiş keçərli bir` **BossJSON** `faylı verin!`")
 
             try:
                 dosya = loads(open(dosya, "r").read())
             except JSONDecodeError:
-                return await event.edit("`Lütfen geçerli bir` **MiaJSON** `dosyası verin!`")
+                return await event.edit("`Xahiş keçərli bir` **BossJSON** `faylı verin!`")
 
-            await event.edit(f"`{dosya['LANGUAGE']}` `dili yükleniyor...`")
+            await event.edit(f"`{dosya['LANGUAGE']}` `dili yüklənir...`")
             pchannel = await event.client.get_entity(PLUGIN_CHANNEL_ID)
 
             dosya = await reply.download_media(file="./userbot/language/")
@@ -34,32 +34,32 @@ async def dil(event):
             await reply.forward_to(pchannel)
             
             LANGUAGE_JSON = dosya
-            await event.edit(f"✅ `{dosya['LANGUAGE']}` `dili başarıyla yüklendi!`\n\n**İşlemlerin geçerli olması için botu yeniden başlatın!**")
+            await event.edit(f"✅ `{dosya['LANGUAGE']}` `dili Uğurla yüklendi!`\n\n**İşləmlərin keçərli olması üçün botu yenidən başladın!**")
         else:
-            await event.edit("**Lütfen bir dil dosyasına yanıt verin!**")
-    elif search(r"bilgi|info", komut):
-        await event.edit("`Dil dosyası bilgileri getiriliyor... Lütfen bekleyiniz.`")
+            await event.edit("**Xahiş bir dil faylına yanıt verin!**")
+    elif search(r"məlumat|info", komut):
+        await event.edit("`Dil faylı məlumatları gətirilir... Xahiş gözlə.`")
         if event.is_reply:
             reply = await event.get_reply_message()
             if ((len(reply.file.name.split(".")) >= 1) and (not reply.file.name.split(".")[1] == "sirijson")):
-                return await event.edit("`Lütfen geçerli bir` **MiaJSON** `dosyası verin!`")
+                return await event.edit("`Xahiş keçərli bir` **BossJSON** `faylı verin!`")
 
             dosya = await reply.download_media()
 
             try:
                 dosya = loads(open(dosya, "r").read())
             except JSONDecodeError:
-                return await event.edit("`Lütfen geçerli bir` **MiaJSON** `dosyası verin!`")
+                return await event.edit("`Xahiş keçərli bir` **BodsJSON** `faylı verin!`")
 
             await event.edit(
                 f"**Dil: **`{dosya['LANGUAGE']}`\n"
                 f"**Dil Kodu: **`{dosya['LANGCODE']}`\n"
                 f"**Çevirmen: **`{dosya['AUTHOR']}`\n"
 
-                f"\n\n`Dil dosyasını yüklemek için` `.dil yükle` `yazın`"
+                f"\n\n`Dil faylını yükləmək üçün` `.dil yükle` `yazın`"
             )
         else:
-            await event.edit("**Lütfen bir dil dosyasına yanıt verin!**")
+            await event.edit("**Lütfen bir dil faylına yanıt verin!**")
     else:
         await event.edit(
             f"**🪙 Dil: **`{LANGUAGE_JSON['LANGUAGE']}`\n"
@@ -68,9 +68,9 @@ async def dil(event):
         )
 
 CmdHelp('dil').add_command(
-    'dil', None, 'Yüklediğiniz dil hakkında bilgi verir.'
+    'dil', None, 'Yüklədiyiniz dil haqqında məlumat verir.'
 ).add_command(
-    'dil bilgi', None, 'Yanıt verdiğiniz dil dosyası hakkında bilgi verir.'
+    'dil məlumat', None, 'Yanıt verdiyiniz dil faylı haqqında məlumat verir.'
 ).add_command(
-    'dil yükle', None, 'Yanıt verdiğiniz dil dosyasını yükler.'
+    'dil yükle', None, 'Yanıt verdiyiniz dil faylını yüklüyər.'
 ).add()
