018608e48728f86000e64faa49cb818f786b5085
bossuserb <103330740+bossuserb@users.noreply.github.com>
2022-04-13 12:42:37 +0400

    Update indir.py

--- userbot/modules/indir.py
+++ userbot/modules/indir.py
@@ -4,21 +4,21 @@ from userbot.events import register
 from userbot.cmdhelp import CmdHelp
 from userbot import bot
 
-@register(outgoing=True, pattern="^.indir ?(.*)")
-async def sirinsta(event):
+@register(outgoing=True, pattern="^.endir ?(.*)")
+async def bossinsta(event):
     if event.fwd_from:
         return
     if not event.reply_to_msg_id:
-        await event.edit("`Yüklemek için Link Verin`")
+        await event.edit("`Yükləmək üçün Link Verin`")
         return
     reply_message = await event.get_reply_message()
     if not reply_message.text:
-        await event.edit("`Yüklemek İçin Link Verin`")
+        await event.edit("`Yükləmək üçün Link Verin`")
         return
     chat = "@SaveAsbot"
     reply_message.sender
     if reply_message.sender.bot:
-        await event.edit("`Siri İndiremedi Bazı Hatalar Nedeniyle Başka Link Dene Be Tatlım ✓`")
+        await event.edit("`Boss Endirmədi Bəzi Xətalar Nədəniylə Başqa Link Cəhd et Də Ürəyim ✓`")
         return
     asc = await event.edit("`Siri Yüklüyor Sabırlı Ol...`")
     async with event.client.conversation(chat) as conv:
@@ -29,26 +29,26 @@ async def sirinsta(event):
             await event.client.send_message(chat, reply_message)
             response = await response
         except YouBlockedUserError:
-            await event.edit(" @SaveAsBot `botunun Engelini Kaldırın Tekrar Deneyin")
+            await event.edit(" @SaveAsBot `botunun Blokunu Qaldırın Təkrar Cəhd edin")
             return
         if response.text.startswith("Forward"):
             await event.edit(
-                "`Gizlilik ayarlarınızdakı ileti kismini düzeltin.`"
+                "`Gizlilik ayarlarınızdakı iləti qismini düzəldin.`"
             )
         elif "Что поддерживается?" in response.text:
             await event.edit(
-                "⛔️ `Bu linkin ne olduğu hakkında bir fikrim yok!`"
+                "⛔️ `Bu linkin nə olduğu haqqında bir fikrim yoxdu!`"
             )
         else:
             await event.delete()
             await event.client.send_file(
                 event.chat_id,
                 response.message.media,
-                caption=f"@SiriUserBot `ile yüklendi`",
+                caption=f"@BossUserBot `ilə yükləndi`",
             )
             await event.client.send_read_acknowledge(conv.chat_id)
             
 
-CmdHelp('indir').add_command(
-    'indir', None, 'Linki Yanıtlayın .indir Komutu İle\nİnstagramdan IGTV-Hikaye-Video-Fotoğraf\nTikToktan Video\nPinterestten Video-Fotoğraf'
+CmdHelp('endir').add_command(
+    'endir', None, 'Linki Yanıtlayın .indir əmri İlə\nİnstagramdan IGTV-Hikaye-Video-Şəkil\nTikTokdan Video\nPinteresttən Video-Şəkil'
 ).add()
