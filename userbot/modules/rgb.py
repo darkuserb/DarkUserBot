be5b7deb6ba718f298109228f3f3147fa116ddd1
bossuserb <103330740+bossuserb@users.noreply.github.com>
2022-04-13 20:00:02 +0400

    Update rgb.py

--- userbot/modules/rgb.py
+++ userbot/modules/rgb.py
@@ -104,5 +104,5 @@ async def get_font_file(client, channel_id):
     return await client.download_media(font_file_message)
 
 CmdHelp('rgb').add_command(
-    'rbg', '<yanıt>', 'Metninizi RGB çıkartmaya dönüştürün.'
+    'rbg', '<yanıt>', 'Mətninizi RGB stikerinə döndərin.'
 ).add()
