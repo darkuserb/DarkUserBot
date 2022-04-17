6d1d03af008bcc9263dbb050f388632f51dd5cee
bossuserb <103330740+bossuserb@users.noreply.github.com>
2022-04-17 20:15:59 +0400

    Update updater.py

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -17,7 +17,7 @@ import sys
 from git import Repo
 from git.exc import GitCommandError, InvalidGitRepositoryError, NoSuchPathError
 
-from userbot import CMD_HELP, HEROKU_APIKEY, HEROKU_APPNAME, UPSTREAM_REPO_URL, ASISTAN, MYID, AUTO_UPDATE
+from userbot import CMD_HELP, HEROKU_APIKEY, HEROKU_APPNAME, UPSTREAM_REPO_URL, BRAIN_CHECKER, ASISTAN, MYID, AUTO_UPDATE
 from userbot.events import register
 from userbot.cmdhelp import CmdHelp
 
