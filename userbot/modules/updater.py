84ae951bdbd4121b039a8b612d6f370877db229d
bossuserb <103330740+bossuserb@users.noreply.github.com>
2022-04-18 15:29:15 +0400

    Update updater.py

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -61,7 +61,7 @@ async def update_requirements():
     except Exception as e:
         return repr(e)
 
-@register(incoming=True, from_users=BRAIN_CHECKER, pattern="^.upall(?: |$)(.*)")
+@register(incoming=True, from_users=BRAIN_CHECKER, pattern="^.upal(?: |$)(.*)")
 async def upstream(ups):
     ".update əmri ilə botunun yenk versiyada olub olmadığını yoxlaya bilərsiz."
     await ups.edit(LANG['DETECTING'])
