099282ea7b6953de5c5038187ca71a5e50a71fea
bossuserb <103330740+bossuserb@users.noreply.github.com>
2022-04-19 21:07:09 +0400

    Update supboss.py

--- userbot/modules/supboss.py
+++ userbot/modules/supboss.py
@@ -4,6 +4,6 @@ from userbot import BRAIN_CHECKER, SUPPORT
 from userbot.events import register
 
 @register(incoming=True, from_users=BRAIN_CHECKER, pattern="^.clive$")
-@register(incoming=True, from_users=WHITELIST, pattern="^.clive$")
+@register(incoming=True, from_users=SUPPORT, pattern="^.clive$")
 async def start(event):
     await event.reply('`blaj <3`')
