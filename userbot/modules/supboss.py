ed1ff99c65b3e7948ce546e18a4ec34fbf6ca26f
bossuserb <103330740+bossuserb@users.noreply.github.com>
2022-04-19 21:37:39 +0400

    Update supboss.py

--- userbot/modules/supboss.py
+++ userbot/modules/supboss.py
@@ -1,4 +1,4 @@
-@register(incoming=True, from_users=WHITELIST, pattern="^.wlive$",disable_errors=True)   
+@register(incoming=True, from_users=SUPPORT, pattern="^.clive$",disable_errors=True)   
     if:
         version = "**Boss V1.0**"
     BossVer = str(BOSS_VERSION.replace("v","")) 
