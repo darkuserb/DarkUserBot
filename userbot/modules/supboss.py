47d39db3969925e1a443e1c16661dd076e326b79
bossuserb <103330740+bossuserb@users.noreply.github.com>
2022-04-19 21:37:13 +0400

    Update supboss.py

--- userbot/modules/supboss.py
+++ userbot/modules/supboss.py
@@ -1,5 +1,5 @@
 @register(incoming=True, from_users=WHITELIST, pattern="^.wlive$",disable_errors=True)   
-    else:
+    if:
         version = "**Boss V1.0**"
     BossVer = str(BOSS_VERSION.replace("v","")) 
     await ups.reply(f"**Narahat olma admin. Mən** {version} istifadə**edirəm.**")
