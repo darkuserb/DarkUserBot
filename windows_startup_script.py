87b1d3c68e0887180c6696de488122128aaf6705
bossuserb <103330740+bossuserb@users.noreply.github.com>
2022-06-27 23:13:11 +0400

    Update windows_startup_script.py

--- windows_startup_script.py
+++ windows_startup_script.py
@@ -1,6 +1,6 @@
 from telethon import TelegramClient
 API_KEY="19285481"
 API_HASH="bdd56fa1b1d84d056b2aeab4718b73d8"
-#oğurlyanann var yoxunu userator admins və boss admins siksi
+#oğurlyanann var yoxunu dark admins siksin
 bot = TelegramClient('userbot',API_KEY,API_HASH)
 bot.start()
